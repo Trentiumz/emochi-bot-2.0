cc1bc7a83c08a795924a78f27dd63832fe3ee6ad
Daniel Ye <danielyeqt@gmail.com>
2021-05-13 21:28:16 -0400

    Optimize to Add emotes

--- tools.py
+++ tools.py
@@ -29,5 +29,14 @@ async def replace_emote(to_remove: discord.Emoji, new_name: str, new_image: byte
     await to_remove.delete()
     return await guild.create_custom_emoji(name=new_name, image=new_image)
 
+async def add_emotes(emotes: list, guild: discord.Guild):
+    limit = guild.emoji_limit
+    # add all emotes possible
+    for i in range(min(len(emotes), limit - len(guild.emojis))):
+        await guild.create_custom_emoji(name=emotes[i][0], image=emotes[i][1])
+    # replace all of the remaining emotes
+    for i in range(min(len(emotes), limit - len(guild.emojis)), len(emotes)):
+        await replace_emote(guild.emojis[i], emotes[i][0], emotes[i][1])
+
 def image_at(url: str):
     return BytesIO(requests.get(str(url)).content).read()
