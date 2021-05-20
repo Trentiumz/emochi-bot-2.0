e6e5e3a7d283cd5072836b2dd6ed2e7297876818
Daniel Ye <danielyeqt@gmail.com>
2021-05-20 09:41:33 -0400

    Replace Links With Files

--- emote_detection.py
+++ emote_detection.py
@@ -3,18 +3,15 @@ import asyncio
 import tools
 import re
 
-# links: {name: link}
-links = {x.split("|")[0]: x.split("|")[1].rstrip() for x in open("./emoji_links.txt", "rt").readlines()}
-
 # list of emotes but formatted for regex
-emotes = [f":{s}:" for s in links]
+emotes = [f":{s.split('.')[0]}:" for s in tools.emote_file_name_list]
 
 # a regex for which we can find all of the possible emotes
 regex_for_emotes = "|".join(emotes)
 
 
 # find what emote names we need
-def needed_emotes(message: str):
+def needed_emotes(message: str) -> list:
     # remove emotest that are already processed
     message = re.sub("<:.+:\d+>|<a:.+:\d+>", "", message)
     # find all remaining wanted emotes (that we have)
@@ -35,29 +32,28 @@ async def send_webhook(message: discord.Message):
     webhook_updating = True
     guild: discord.Guild = message.guild
 
-
     # get the emotes to replace, all just pure names of the emotes
     needed = list(needed_emotes(message.content))
     if len(needed) == 0:
+        webhook_updating = False
         return
     # if he needs too many emotes, then boop we quit it
-    if len(needed) > guild.emoji_limit // 3:
+    if len(needed) > guild.emoji_limit // 5:
         tools.webhook_empty(
             f"Turns out the server doesn't have enough emote slots to hold your {len(needed)} emotes!")
+        webhook_updating = False
         return
 
-
     # update emotes in the server
-    loaded_emotes = await tools.add_emotes(needed, guild, links)
-
+    loaded_emotes = await tools.add_emotes(set(needed), guild)
 
     # update the message to replace it all with updated emotes
     k = message.content
     for emote_name in loaded_emotes:
         k = re.sub(f":{emote_name}:", str(loaded_emotes[emote_name]), k)
 
-
     # send the new message & delete original
     tools.webhook_imitate(k, message.author)
-    webhook_updating = False
+
     await message.delete()
+    webhook_updating = False
--- tools.py
+++ tools.py
@@ -1,12 +1,20 @@
 import requests
 import re
-from io import BytesIO
+import os
 import discord
 
 lines = {x.split(": ")[0]: x.split(": ")[1] for x in open("./info.txt", "rt").readlines()}
 
-def get_hook_url():
+emote_path = "./data/emotes/"
+emote_file_name_list = os.listdir(emote_path)
+# emote_file_names[ending] = set of files with that ending
+emote_file_names = {"png": set([x for x in emote_file_name_list if re.match(".*\.png", x)]),
+                    "gif": set([x for x in emote_file_name_list if re.match(".*\.gif", x)])}
+
+
+def get_hook_url() -> str:
     return lines["hook_url"]
+
 
 # send webhook imitating another user
 def webhook_imitate(message: str, user: discord.User):
@@ -19,6 +27,7 @@ def webhook_imitate(message: str, user: discord.User):
 
     requests.post(hook_url, json=obj)
 
+
 # send an empty webhook
 def webhook_empty(message: str):
     hook_url = get_hook_url()
@@ -27,35 +36,52 @@ def webhook_empty(message: str):
     }
     requests.post(hook_url, json=obj)
 
+
 # replace the current emote with a new one
-async def replace_emote(to_remove: discord.Emoji, new_name: str, new_image: bytes):
+async def replace_emote(to_remove: discord.Emoji, new_name: str, new_image: bytes) -> discord.Emoji:
     guild: discord.Guild = to_remove.guild
     await to_remove.delete()
     return await guild.create_custom_emoji(name=new_name, image=new_image)
 
-# gets the image at some url
-def image_at(url: str):
-    return BytesIO(requests.get(str(url)).content).read()
 
-# emotes: [(name, image)]
-async def add_emotes(emotes: list, guild: discord.Guild, links: dict):
+async def add_emote(name: str, image: bytes, guild: discord.Guild) -> discord.Emoji:
+    return await guild.create_custom_emoji(name=name, image=image)
+
+
+def image_at(emote_name: str) -> bytes:
+    for ending in emote_file_names:
+        if f"{emote_name}.{ending}" in emote_file_names[ending]:
+            return open(f"{emote_path}{emote_name}.{ending}", "rb").read()
+
+
+# emotes: list of emote names
+async def add_emotes(emote_names: set, guild: discord.Guild) -> dict:
     limit = guild.emoji_limit
     guild_emotes = guild.emojis
     # index we should start to replace instead of add emotes
-    start_replace_ind = min(len(emotes), limit - len(guild_emotes))
-    ids = {}
+    num_to_add = (limit - len(guild_emotes))
+    emotes = {}
+    existing_emotes = {str(x.name): x for x in guild_emotes}
+
+    # make sure to replace existing emotes
+    for i in emote_names:
+        if i in existing_emotes:
+            emotes[i] = existing_emotes[i]
+    for i in emotes:
+        emote_names.remove(i)
+
+    # get what else we still need to add
+    emote_names = list(emote_names)
+    to_add = emote_names[:num_to_add]
+    to_replace = emote_names[num_to_add:]
 
     # add all emotes possible
-    existing_emotes = {str(x.name): x for x in guild_emotes}
-    for i in range(start_replace_ind):
-        if emotes[i] in existing_emotes:
-            ids[emotes[i]] = existing_emotes[emotes[i]]
-        else:
-            ids[emotes[i]] = await guild.create_custom_emoji(name=emotes[i], image=image_at(links[emotes[i]]))
+    for i in to_add:
+        emotes[i] = await guild.create_custom_emoji(name=i, image=image_at(i))
 
     # replace all of the remaining emotes
-    for i in range(start_replace_ind, len(emotes)):
-        ids[emotes[i]] = await replace_emote(guild_emotes[i - start_replace_ind], emotes[i], image_at(links[emotes[i]]))
+    for i in range(len(to_replace)):
+        emotes[emote_names[i]] = await replace_emote(guild_emotes[i], emote_names[i], image_at(emote_names[i]))
 
-    # ids: {name: emote object}
-    return ids
+    # emotes: {name: emote object}
+    return emotes
