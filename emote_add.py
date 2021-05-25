08c1272fb7fa085f943c65595a9fd77b2f867ecd
Daniel Ye <danielyeqt@gmail.com>
2021-05-25 17:28:23 -0400

    Not consider Picture as Gif

--- emote_add.py
+++ emote_add.py
@@ -11,18 +11,21 @@ def get_image(url: str) -> Image:
         f.write(part.content)
     return Image.open("./data/temp.gif")
 
-def rescale_image(image: Image) -> Image:
+def rescale_image(image: Image) -> list:
     width, height = image.size
     scale_down = min(100 / width, 100 / height)
 
-    frames = []
-    first_frame = True
-    for f in ImageSequence.Iterator(image):
-        f.info["transparency"] = f.info["transparency"] if first_frame else 0
-        first_frame = False
-        frames.append(f.resize((int(width * scale_down), int(height * scale_down))).convert("RGBA"))
+    if image.n_frames == 1:
+        return [image.resize((int(width * scale_down), int(height * scale_down))).convert("RGBA")]
+    else:
+        frames = []
+        first_frame = True
+        for f in ImageSequence.Iterator(image):
+            f.info["transparency"] = f.info["transparency"] if first_frame else 0
+            first_frame = False
+            frames.append(f.resize((int(width * scale_down), int(height * scale_down))).convert("RGBA"))
 
-    return frames
+        return frames
 
 def save_image(frames: list, name: str):
     if len(frames) > 1:
