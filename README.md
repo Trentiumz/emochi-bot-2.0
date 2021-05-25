41b5984edb2e8fe84c9ea70334605ec03449a275
Daniel Ye <danielyeqt@gmail.com>
2021-05-25 17:23:47 -0400

    Handle transparent emotes

--- README.md
+++ README.md
@@ -23,6 +23,8 @@ This bot now works, but there are still a few optimizations and functionality to
 
 I would like to give credit to Paul (github at https://github.com/hillcrestpaul0719) for the main brute of the theory behind adding discord emojis into messages. Don't worry tho - I still coded all of the code here, though I did use a few api's
 
+Furthermore, I copied the pillow workaround to save a transparent gif file properly from https://stackoverflow.com/questions/60948028/python-pillow-transparent-gif-isnt-working, which adapted code from https://github.com/python-pillow/Pillow/issues/4644
+
 I would also like to thank my classmates for a giant database of emotes which I could ~~pirate~~ borrow for this bot. So, without further ado, thank you: 
 - Shane Chen (https://github.com/mars-flat)
 - Aaron Zhu (https://github.com/AAZZAZRON)
--- emote_add.py
+++ emote_add.py
@@ -1,7 +1,9 @@
 from PIL import Image
+from gif_saving import save_transparent_gif
 from PIL import ImageSequence
 import tools
 import requests
+import numpy
 
 def get_image(url: str) -> Image:
     part = requests.get(url)
@@ -14,14 +16,17 @@ def rescale_image(image: Image) -> Image:
     scale_down = min(100 / width, 100 / height)
 
     frames = []
+    first_frame = True
     for f in ImageSequence.Iterator(image):
-        frames.append(f.resize((int(width * scale_down), int(height * scale_down))))
+        f.info["transparency"] = f.info["transparency"] if first_frame else 0
+        first_frame = False
+        frames.append(f.resize((int(width * scale_down), int(height * scale_down))).convert("RGBA"))
 
     return frames
 
 def save_image(frames: list, name: str):
     if len(frames) > 1:
-        frames[0].save(f"./data/emotes/{name}.gif", append_images=frames[1:], save_all=True)
+        save_transparent_gif(frames, [int(x.info["duration"]) for x in frames], f"./data/emotes/{name}.gif")
         tools.saved_emote_update(f"{name}.gif")
     else:
         frames[0].save(f"./data/emotes/{name}.png")
